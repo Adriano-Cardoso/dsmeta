d00cea96cb6378bbb8bb41de66bee4d691828982
Adriano Santos Goulart Cardoso <adrianogc@ciandt.com>
2022-11-14 12:11:33 -0300

    primeiro commit

---
+++ dsmeta-backend/src/main/java/com/ds/dsmetabackend/DsmetaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ds.dsmetabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaBackendApplication.class, args);
+	}
+
+}
