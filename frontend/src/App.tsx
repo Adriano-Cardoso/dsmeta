bb6bed27f56ed943281732ca6307bf645e1e7b40
Adriano Santos Goulart Cardoso <adrianogc@ciandt.com>
2022-11-16 22:04:17 -0300

    finalizando front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
 
+import Header from "./componets/Header";
+import SalesCard from "./componets/SalesCard";
+
+
 function App() {
   return (
-    <h1>Olá mundo</h1>
+    <>
+      <Header/>
+      <main>
+        <section id={"sales"}>
+          <div className="dsmeta-container">
+
+            <SalesCard/>
+
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
-export default App
+export default App;
